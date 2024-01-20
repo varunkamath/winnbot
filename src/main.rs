190dcb45db125abfb0ff9acabc0da336d313a2ea
Varun Kamath <varun.kamath@gmail.com>
2024-01-19 18:05:15 -0600

    Bugfix / cleanup

--- src/main.rs
+++ src/main.rs
@@ -86,40 +86,40 @@ impl EventHandler for Handler {
                     println!("Error sending message: {:?}", why);
                 }
             }
-            let mudae_id = env::var("MUDAE_ID")
-                .expect("Failed to get MUDAE_ID from the environment variables");
-            if msg.author.id == mudae_id.parse::<u64>().unwrap() {
-                if let Some(embed) = msg.embeds.first() {
-                    let name = embed.author.as_ref().unwrap().name.clone();
-                    let data = include_str!("data.txt");
-                    let mut line_number = 0;
-                    let mut in_list = false;
-                    // If the name is in the list
-                    for line in data.lines() {
-                        line_number += 1;
-                        // Fuzzy match entire line
-                        if line.to_lowercase() == name.to_lowercase() {
-                            in_list = true;
-                            let user_id = env::var("USER_ID")
-                                .expect("Failed to get USER_ID from the environment variables");
-                            let user = UserId::new(user_id.parse::<u64>().unwrap());
-                            if let Err(why) = user
-                                .create_dm_channel(&ctx.http)
-                                .await
-                                .unwrap()
-                                .say(
-                                    &ctx.http,
-                                    format!("{} is number {} in the list", name, line_number),
-                                )
-                                .await
-                            {
-                                println!("Error sending message: {:?}", why);
-                            }
+        }
+        let mudae_id =
+            env::var("MUDAE_ID").expect("Failed to get MUDAE_ID from the environment variables");
+        if msg.author.id == mudae_id.parse::<u64>().unwrap() {
+            if let Some(embed) = msg.embeds.first() {
+                let name = embed.author.as_ref().unwrap().name.clone();
+                let data = include_str!("data.txt");
+                let mut line_number = 0;
+                let mut in_list = false;
+                // If the name is in the list
+                for line in data.lines() {
+                    line_number += 1;
+                    // Fuzzy match entire line
+                    if line.to_lowercase() == name.to_lowercase() {
+                        in_list = true;
+                        let user_id = env::var("USER_ID")
+                            .expect("Failed to get USER_ID from the environment variables");
+                        let user = UserId::new(user_id.parse::<u64>().unwrap());
+                        if let Err(why) = user
+                            .create_dm_channel(&ctx.http)
+                            .await
+                            .unwrap()
+                            .say(
+                                &ctx.http,
+                                format!("{} is number {} in the list", name, line_number),
+                            )
+                            .await
+                        {
+                            println!("Error sending message: {:?}", why);
                         }
                     }
-                    if !in_list {
-                        println!("{} is not in the list", name);
-                    }
+                }
+                if !in_list {
+                    println!("{} is not in the list", name);
                 }
             }
         }
